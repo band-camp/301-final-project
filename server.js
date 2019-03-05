6853cd808910514325663448b0e0ef80f968c01d
cmorto02 <chrismorton.cm@gmail.com>
2019-03-04 17:01:29 -0800

    trying to get the api data

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+'use strict';
+
 //Dependencies
+
 const express = require('express');
 const pg = require('pg');
 const superagent = require('superagent');
+
+//Application setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
@@ -14,15 +19,30 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
-//Routes
-app.get('hello', proofOfLife);
-
-
-
-
-
-
-
+//Similar Artists Route
+app.post('/search-results', loadSimilarArtists);
 
 //Server listening to requests on PORT
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+
+//Ticketmaster Route
+
+// Catch-all route that renders the error page
+app.get('*', (request, response) => response.status(404).render('pages/error'));
+
+function Band(info){
+  this.bandname = info.results.name;
+}
+
+// Searches route handler
+function loadSimilarArtists(request, response) {
+  let url = `https://tastedive.com/api/similar?q=${request.body.search}&type=music&limit=9&k=${process.env.TASTE_DIVE_API_KEY}`
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Band(bookResult.volumeInfo)))
+    .then(results => {
+      response.render('pages/searches/show', { searchesResults: results });
+    })
+}
+
